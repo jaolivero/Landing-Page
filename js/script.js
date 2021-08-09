760d9e06492cbf2124a735bf698621a2280f906b
Joel Olivero <joelolivero@outlook.com>
2021-08-09 16:43:11 -0400

    fixed nav visibility for mobile

--- js/script.js
+++ js/script.js
@@ -2,27 +2,27 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelectorAll('.has-fade');
+const fadeElems = document.querySelectorAll('.has-fade');
 
-btnHamburger.addEventListener('click', function() {
-    console.log('open hambuger');
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
 
-    if(header.classList.contains('open')) {
-        header.classList.remove('noscroll');
-        header.classList.remove('open');
-        
-        fadeElements.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
-        });
-    }
-    else { 
-        body.classList.add('noscroll');
-        header.classList.add('open');
-        
-        fadeElements.forEach(function(element) {
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
     });
+    
   }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
 });
